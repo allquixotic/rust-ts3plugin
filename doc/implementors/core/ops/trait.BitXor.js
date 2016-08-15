5c1c0975b8ab18c9634596c7ca88d6adc13c8998
Flakebi <flakebi@t-online.de>
2016-08-15 21:21:16 +0200

    Update documentation

---
+++ doc/implementors/core/ops/trait.BitXor.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ts3plugin_sys"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a> for <a class='struct' href='ts3plugin_sys/public_definitions/struct.Speaker.html' title='ts3plugin_sys::public_definitions::Speaker'>Speaker</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a> for <a class='struct' href='ts3plugin_sys/public_definitions/struct.BBCodeTags.html' title='ts3plugin_sys::public_definitions::BBCodeTags'>BBCodeTags</a>",];implementors["ts3plugin"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;<a class='struct' href='ts3plugin_sys/public_definitions/struct.Speaker.html' title='ts3plugin_sys::public_definitions::Speaker'>Speaker</a>&gt; for <a class='struct' href='ts3plugin_sys/public_definitions/struct.Speaker.html' title='ts3plugin_sys::public_definitions::Speaker'>Speaker</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;<a class='struct' href='ts3plugin_sys/public_definitions/struct.BBCodeTags.html' title='ts3plugin_sys::public_definitions::BBCodeTags'>BBCodeTags</a>&gt; for <a class='struct' href='ts3plugin_sys/public_definitions/struct.BBCodeTags.html' title='ts3plugin_sys::public_definitions::BBCodeTags'>BBCodeTags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
