a90d70b34357a0587d814b78c0e75362041d8fcf
Flakebi <flakebi@t-online.de>
2016-09-07 22:28:21 +0200

    Update documentation

---
+++ doc/implementors/core/ops/trait.RemAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ts3plugin"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
